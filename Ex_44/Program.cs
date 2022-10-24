8d19a63f964c3baa4b6651b38e07691f1d526c51
Mark <kalmark@mail.ru>
2022-10-24 23:55:14 +0300

    Add Ex_44 Ex_45

---
+++ Ex_44/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int first = 0;
+int second = 1;
+int next = 0;
+Console.Write("0 ");
+if (a > 1) Console.Write("1 ");
+for (int i = 3; i <= a; i++)
+{
+        next = first + second;
+        Console.Write($"{next} ");
+        first = second;
+        second = next;
+}
---
+++ Ex_45/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 45: 
+// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования.
+
+int len = 6;
+int min = 0;
+int max = 10;
+
+int[] rand = FillArray(len, min, max);
+PrintArray(rand);
+int[] copyrand = CopyArray(rand);
+PrintArray(copyrand);
+
+int[] FillArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+int[] CopyArray(int[] array)
+{
+    int n = array.Length;
+    int[] newarray = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        newarray[i] = array[i];
+    }
+    return newarray;
+}
+
+void PrintArray(int[] array)
+{
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1) Console.WriteLine($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+}
