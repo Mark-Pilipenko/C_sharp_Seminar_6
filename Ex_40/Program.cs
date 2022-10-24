fb8da73906082fbe4bf711f28896cfcbe7e7eaa2
Mark <kalmark@mail.ru>
2022-10-24 23:22:28 +0300

    Add Ex_40_42

---
+++ Ex_40/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 40: 
+// Напишите программу, которая принимает на вход три числа и проверяет, 
+// может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон.
+
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a + b > c && a + c > b && b + c > a) Console.WriteLine("YES!");
+else Console.WriteLine("No(((");
---
+++ Ex_42/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+To2(a);
+
+void To2(int numb)
+{
+    string numb2 = string.Empty;
+    while (numb != 0)
+    {
+        numb2 = numb2 + numb % 2;
+        numb = numb / 2;
+    }
+    for (int i = numb2.Length - 1; i >= 0; i--)
+    {
+        Console.Write(numb2[i]);
+    }
+}
+
