0be55b6b6f9dc475fc3733b1a15171644272208e
Mark <kalmark@mail.ru>
2022-10-24 22:30:14 +0300

    Add Ex_39

---
+++ Ex_39/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 39: 
+// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+int len = 6;
+int min = 0;
+int max = 10;
+
+int[] rand = FillArray(len, min, max);
+PrintArray(rand);
+Reverse(rand);
+PrintArray(rand);
+
+int[] FillArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+    {
+        if (i == n - 1) Console.WriteLine($"{array[i]}");
+        else Console.Write($"{array[i]}, ");
+    }
+}
+
+void Reverse(int[] array)
+{
+    int temp = 0;
+    for (int i = 0; i < array.Length / 2; i++)
+    {temp = array[i];
+    array[i] = array[array.Length - i - 1];
+    array[array.Length - i - 1] = temp;
+    }
+}
